0879346008da479f8b47bd90d986e2548a404327
devankitx <ankitmaurya.coder@gmail.com>
2026-02-11 09:04:38 +0530

    types in hooks

--- React-Ts/src/Person.tsx
+++ React-Ts/src/Person.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
+
 export interface persons {
   name: string;
   age: number;
   isMarried: boolean;
 }
 export const Person = (props: persons) => {
+  const [isShowInfo, setShowInfo] = useState<boolean>(false);
+  const toggleInfo = () => {
+    setShowInfo((prev) => !prev);
+  };
   return (
     <>
-      <p>Name :{props.name}</p>
-      <p>Age : {props.age}</p>
-      <p>
-        This person is{" "}
-        {props.isMarried ? "yes he is married" : "no he is single"}
-      </p>
+      {isShowInfo && (
+        <>
+          <p>Name :{props.name}</p>
+          <p>Age : {props.age}</p>
+          <p>This person is {props.isMarried ? " married" : " single"}</p>
+        </>
+      )}
+      <button onClick={toggleInfo}>Toggle Info</button>
     </>
   );
 };
