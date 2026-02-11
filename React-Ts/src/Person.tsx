756d0dc4338d2e0820341498eab8d5469237ec84
devankitx <ankitmaurya.coder@gmail.com>
2026-02-11 09:21:15 +0530

    did types in react

--- React-Ts/src/Person.tsx
+++ React-Ts/src/Person.tsx
@@ -6,20 +6,28 @@ export interface persons {
   isMarried: boolean;
 }
 export const Person = (props: persons) => {
-  const [isShowInfo, setShowInfo] = useState<boolean>(false);
-  const toggleInfo = () => {
-    setShowInfo((prev) => !prev);
+  // const [isShowInfo, setShowInfo] = useState<boolean>(false);
+  // const toggleInfo = () => {
+  //   setShowInfo((prev) => !prev);
+  // };
+  const [personBio, setPersonBio] = useState<string | null>(null);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPersonBio(event.target.value);
   };
   return (
     <>
-      {isShowInfo && (
-        <>
-          <p>Name :{props.name}</p>
-          <p>Age : {props.age}</p>
-          <p>This person is {props.isMarried ? " married" : " single"}</p>
-        </>
-      )}
-      <button onClick={toggleInfo}>Toggle Info</button>
+      <div>
+        <h2>Name :{props.name}</h2>
+        <h2>Age : {props.age}</h2>
+        <h2>This person is {props.isMarried ? " married" : " single"}</h2>
+        <h2>
+          {props.name}-Bio: {!personBio ? "No bio" : personBio}
+        </h2>
+
+        <p>Type in input box below to add person bio</p>
+        <input onChange={handleChange} type="text" name="" id="Bio" />
+        {/* <button onClick={toggleInfo}>Toggle Info</button> */}
+      </div>
     </>
   );
 };
